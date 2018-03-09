b7b63216a847dfe5dc217f1960bb21c7a81c3879
Asetr <asetre.paul@gmail.com>
2018-03-09 01:31:23 -0800

    realtime forms

--- src/App.js
+++ src/App.js
@@ -22,12 +22,13 @@ class App extends Component {
         }
 
         this.handleCreateForm = this.handleCreateForm.bind(this)
+        this.handleFormChange = this.handleFormChange.bind(this)
     }
 
     componentDidMount() {
         FormDB.on('value', snap => {
             const forms = snap.val()
-            this.setState(forms)
+            this.setState({...this.state, forms})
         })
     }
 
@@ -47,6 +48,19 @@ class App extends Component {
         return FormDB.update(updates)
     }
 
+    handleFormChange(e, key) {
+        const name = e.target.name
+        const value = e.target.value
+        const updates = {}
+        updates[name] = value
+        FormDB.child(key).update(updates)
+        /*
+        const title = e.target.title.value
+        const description = e.target.description.value
+        console.log(title, description)
+        */
+    }
+
     render() {
         return (
             <div className="App">
@@ -55,6 +69,17 @@ class App extends Component {
                     <input type="text" name="description" placeholder="description"/>
                     <input type="submit" value="Create form"/>
                 </Form>
+
+                {Object.keys(this.state.forms).map(key => {
+                    const form = this.state.forms[key]
+                    return(
+                        <Form key={key} onChange={event => this.handleFormChange(event, key)}>
+                            <input type="text" name="title" value={form.title} />
+                            <input type="text" name="description" value={form.description}/>
+                            <input type="submit"/>
+                        </Form>
+                    )
+                })}
             </div>
         );
     }

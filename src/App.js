d8ecb8aa50007ff32f4d197c389dc1fde33fbec8
Asetr <asetre.paul@gmail.com>
2018-03-08 22:12:21 -0800

    create forms

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import styled from 'styled-components'
+import firebase from 'firebase'
 
 //Components
-import Form from './form'
+//import Form from './form'
+const Form = styled.form`
+    input {
+        font-size: 24px;
+    }
+`
+
+
+const FormDB = firebase.database().ref('Forms')
 
 class App extends Component {
     constructor() {
         super()
         this.state = {
-            forms: []
+            forms: {}
         }
+
+        this.handleCreateForm = this.handleCreateForm.bind(this)
     }
-    s
+
+    componentDidMount() {
+        FormDB.on('value', snap => {
+            const forms = snap.val()
+            this.setState(forms)
+        })
+    }
+
+    handleCreateForm(e)  {
+        e.preventDefault()
+        const title = e.target.title.value
+        const description = e.target.description.value
+
+        const form = {
+            title,
+            description
+        }
+
+        const newKey = FormDB.push().key
+        const updates = {}
+        updates[newKey] = form
+        return FormDB.update(updates)
+    }
+
     render() {
         return (
             <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <h1 className="App-title">Welcome to React</h1>
-                </header>
+                <Form onSubmit={this.handleCreateForm}>
+                    <input type="text" name="title" placeholder="title"/>
+                    <input type="text" name="description" placeholder="description"/>
+                    <input type="submit" value="Create form"/>
+                </Form>
             </div>
         );
     }
